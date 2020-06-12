aaf42fa3d8896ae7702c510d9703b48514e8a494
Ekin Ceylan <ekin@Ekin-MacBook-Pro.local>
2020-06-12 20:34:48 +0300

    sayı tahmin

---
+++ SayiTahmin/app/src/main/java/com/ekncyln/sayitahmin/SonucActivity.java
@@ -0,0 +1,13 @@
+package com.ekncyln.sayitahmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SonucActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sonuc);
+    }
+}
---
+++ SayiTahmin/app/src/main/java/com/ekncyln/sayitahmin/TahminActivity.java
@@ -0,0 +1,13 @@
+package com.ekncyln.sayitahmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TahminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tahmin);
+    }
+}
